beaa2c646e64214b359779b8431fe0337f99de19
JeffRisberg <j.s.risberg@gmail.com>
2025-03-03 10:29:38 -0800

    take user/password information out of the persistence.xml file

--- src/main/java/com/company/db/EntityManagerFactoryProvider.java
+++ src/main/java/com/company/db/EntityManagerFactoryProvider.java
@@ -4,32 +4,43 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class EntityManagerFactoryProvider {
 
-    private static HikariDataSource ds;
-    static EntityManagerFactory emf = null;
+  private static HikariDataSource ds;
+  static EntityManagerFactory emf = null;
 
-    public static EntityManagerFactory getEMF() {
-        if (emf == null) {
-            HikariConfig config = new HikariConfig("db.properties");
+  public static EntityManagerFactory getEMF() {
+    if (emf == null) {
+      HikariConfig config = new HikariConfig("db.properties");
 
-            String jdbcUrl = (String) config.getDataSourceProperties().get("jdbcUrl");
+      Properties dataSourceProperties = config.getDataSourceProperties();
 
-            config.setJdbcUrl(jdbcUrl);
+      String jdbcUrl = (String) dataSourceProperties.get("jdbcUrl");
+      String user = (String) dataSourceProperties.get("user");
+      String password = (String) dataSourceProperties.get("password");
 
-            config.addDataSourceProperty("cachePrepStmts", "true");
-            config.addDataSourceProperty("prepStmtCacheSize", "250");
-            config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-            ds = new HikariDataSource(config);
+      config.setJdbcUrl(jdbcUrl);
 
-            Map<String, Object> properties = new HashMap();
-            properties.put("datasource", ds);
-            emf = Persistence.createEntityManagerFactory("JPA07", properties);
-        }
-        return emf;
+      config.addDataSourceProperty("cachePrepStmts", "true");
+      config.addDataSourceProperty("prepStmtCacheSize", "250");
+      config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+      ds = new HikariDataSource(config);
+
+      Map<String, Object> properties = new HashMap();
+      properties.put("jakarta.persistence.dataSource", ds);
+      properties.put("jakarta.persistence.jdbc.user", user);
+      properties.put("jakarta.persistence.jdbc.password", password);
+      properties.put("jakarta.persistence.jdbc.url", jdbcUrl);
+
+      properties.put("hibernate.hbm2ddl.auto", "update");
+      properties.put("hibernate.dialect", "org.hibernate.dialect.MariaDBDialect");
+
+      emf = Persistence.createEntityManagerFactory("JPA07", properties);
     }
+    return emf;
+  }
 }
